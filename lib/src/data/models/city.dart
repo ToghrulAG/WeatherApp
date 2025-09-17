c1c07e62bd36f2daaab6cda8de69acc6d3b6b083
Toghrul AG <tgrlagl@proton.me>
2025-09-17 17:52:32 +0400

    deleted id field, added country field

--- lib/src/data/models/city.dart
+++ lib/src/data/models/city.dart
@@ -4,11 +4,12 @@ part 'city.g.dart';
 
 @HiveType(typeId: 0)
 class City extends HiveObject {
+
   @HiveField(0)
-  final int id;
+  final String name;
 
   @HiveField(1)
-  final String name;
+  final String country; // country
 
   @HiveField(2)
   final double lat;
@@ -18,13 +19,14 @@ class City extends HiveObject {
 
   @HiveField(4)
   final String timezone; 
-
+  
   @HiveField(5)
   final int order;
 
   City({
-    required this.id,
+        // deleted id field
     required this.name,
+    required this.country,  /// country
     required this.lat,
     required this.lon,
     required this.timezone,
